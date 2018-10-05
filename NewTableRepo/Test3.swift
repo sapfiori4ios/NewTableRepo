48056d4e2e90850b4ca347a84323891e43235320
Basavaraja <basavaraja@admins-MacBook-Pro.local>
2018-10-05 15:17:33 +0530

    git ignores

---
+++ NewTableRepo/Test3.swift
@@ -0,0 +1,13 @@
+//
+//  Test3.swift
+//  NewTableRepo
+//
+//  Created by Basavaraja on 10/5/18.
+//  Copyright © 2018 fulcrum. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class divide: UITableViewCell{
+    
+}
