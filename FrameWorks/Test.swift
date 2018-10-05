f3ec451a2298d0a8dc47a3adca88abea6a15f8d9
Basavaraja <basavaraja@admins-MacBook-Pro.local>
2018-10-05 14:53:24 +0530

    first commit

---
+++ FrameWorks/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  NewTableRepo
+//
+//  Created by Basavaraja on 10/5/18.
+//  Copyright © 2018 fulcrum. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Add: UITableViewCell {
+    
+}
